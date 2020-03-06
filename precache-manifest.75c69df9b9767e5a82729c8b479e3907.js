2ed36c521840da59faedacd6cd581fc33cef7870
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-03-06 12:52:50 -0400

    Updates

---
+++ precache-manifest.75c69df9b9767e5a82729c8b479e3907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f7dd074699056d452f957d5bf23976",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "fca2ad60887539c1431a",
+    "url": "/markdown-previewer/static/css/main.c63f0fd1.chunk.css"
+  },
+  {
+    "revision": "6c6bc7c87fa7ba8371df",
+    "url": "/markdown-previewer/static/js/2.4cd28fd7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/markdown-previewer/static/js/2.4cd28fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca2ad60887539c1431a",
+    "url": "/markdown-previewer/static/js/main.bb55c408.chunk.js"
+  },
+  {
+    "revision": "699ce588a95e5b83c15e",
+    "url": "/markdown-previewer/static/js/runtime-main.b388dd98.js"
+  }
+]);
